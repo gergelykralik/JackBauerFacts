45debe473413b2c196b059bf9b67a71793c9f83f
Kralik Gergely <kralik.gergely@gmail.com>
2014-07-08 10:16:37 +0200

    JackBauerFacts

---
+++ README.md
@@ -0,0 +1,4 @@
+# JackBauerFacts
+
+A collection of some of the best Jack Bauer facts in Hungarian!
+
---
+++ facts.js
@@ -0,0 +1,95 @@
+var jack_bauer_facts = [
+	"Ha Jack Bauer elmosolyodik, egy terrorista valahol a világon spontán kasztrálódik.",
+	"A Bibliát Jack Bauer írta. Ezért olyan erőszakos.",
+	"Jack Bauer a Lost szigetéről 24 óra alatt meglépne.",
+	"Jack Bauer egyszer elfelejtette, hogy hová tette a kocsikulcsát. Fél órán át kínozta magát, mire megtört, és elárulta.",
+	"Kína teljes lakossága Jack fejét akarja. Fair küzdelem lenne.",
+	"Most csak azért vagyunk eszméletünknél, mert Jack nem akar cipelni minket.",
+	"Ha valaki megöli Jack Bauert, attól még nem hal meg, csak rohadt ideges lesz.",
+	"Az apokalipszisnak eredetileg 5 lovasa volt. Jack inkább gyalog jött.",
+	"Egy átlagember egy napot kihúz víz nélkül. Jack már 5 napja nem ivott egy kortyot sem.",
+	"Amikor Jack lánya elveszítette a szüzességét, Jack megkereste, és visszatette.",
+	"Jack soha nem téveszt. Ha nem talált el, az csak azért van, mert egy 20 km-re mögötted lévő terroristára célzott.",
+	"Gyermekkorában Jack addig kínozta a szüleit, míg meg nem mondták, hogy hová rejtették a karácsonyi ajándékokat.",
+	"Amikor Jacknek elfogyott a lőszere, a tűzvonalba állt, és a testébe fúródó ellenséges golyókkal töltött újra.",
+	"Nostradamus egyszer azt jóésolta, hogy \"eljő egy férfi, Jacques névre hallgat...és ötször menti meg a világot.\" Mire leírta, Jack rárúgta az ajtót, szétlőtte a térdkalácsát, miközbeen ezt üvöltötte: \"KINEK DOLOGOZOL?\"",
+	"Ha Jack Bauer meleg lenne, ő lenne Chuck Norris.",
+	"Jack Bauernek nincsenek barátai, mert a gyerekkori rabló-pandúr után minden rablót megkínzott, és megölt.",
+	"Jack Bauer az egyedüli, aki képes egy repülőt a csomagtérből vezetni.",
+	"Los Angelesben egy utcát neveztek el Jack Bauerről, hálából, amiért megóvta a várost és lakóit. Kisvártatva az utcát át kellett nevezni, mivel mindenki meghalt, aki átkelt a Jack Bauer utcán. Jack Baueren nem lehet csak úgy átjutni.",
+	"Ha Jack Bauer egy szobába kerülne Hitlerrel, Sztálinnal és Nina Myersszel, és a pisztolyában csak két golyó lenne, kit választana? Ninát, kétszer.",
+	"Jack Bauer nem éhes, nem álmos, nincs szüksége fürdésre. A szervezete nem olyan ostoba, hogy felhívja magára a figyelmét.",
+	"Ha a '24'-ben mindenki Jack Bauer utasításait követné, a sorozatot úgy hívhatnák: '12'.",
+	"Valaki egy \"ki kopog\" találós kérdést tett fel jack Bauernek. Ő kitalálta nem csak azt, hogy ki kopog, de azt is, kinek dolgozik, és hová rejtette azt az átkozott bombát.",
+	"Jack Bauer naptárában március 31. után április 2. következik. Senki sem bolondozhat Jack Bauer-rel.",
+	"Egyetlen terrortámadás sem érte az Egyesült Államokat, mióta Jack Bauer feltűnt a televízióban.",
+	"Kalifornia állam azt tervezi, az erőszakos bűncselekmények visszaszorítása érdekében - elrettentésül - a legsúlyosabb büntetési tételt megszigorítja, halálos méreginjekcióról Jack Bauer-ra.",
+	"Ha holnap reggel felébredsz, csak azért van, mert Jack megkímélte az életed.",
+	"Szupermen Jack Bauer-es pizsmát hord.",
+	"Jack Bauer amikor lefekszik aludni, mindig egy párnát tesz a pisztolya alá.",
+	"Jack Bauer úgy játssza az orosz rulettet, hogy teletölti a tárat. Persze, így is nyer.",
+	"Jack Bauer egy drót nélküli telefonnal is képes megfojtani.",
+	"Jack Bauer sosem szokott rá a heroinra. A heroin szokott rá Jack Bauer-ra.",
+	"A Közel-Keleten a férfiak körében a demográfiai statisztikák szerint a leggyakoribb halálozási ok: Jack Bauer.",
+	"A problémákra van jó megoldás, van rossz megoldás, és van Jack Bauer megoldása. Utóbbi lényegében ugyanaz, mint a jó megoldás, csak gyorsabb és több halálesettel jár.",
+	"Jack Bauer 96 óra alatt megölt 93 embert és megmentette a világot - négyszer. Mondd, te mit csináltál egész életedben?!",
+	"Jack Bauer nem vesződik az ujjlenyomattal. Ha egyszer kéznél van az ujj is.",
+	"Jack Bauer nem telepített tűzfalat az otthoni számítógépére. Ő Bauer-falat használ. Ez egy egyszerű jpeg kép saját magáról. Soha nem próbálkoztak vírusok, hogy behatoljanak a gépére. Egy sem.",
+	"Jack Bauer nem beszél idegen nyelveket, de sec perc alatt rábírja a külföldieket, hogy értsenek angolul.",
+	"Jack Bauer és Szupermen egyszer régen szkanderoztak. Az volt a tét: aki veszít, onnantól kívül hordja az alsógatyáját.",
+	"Amikor Jack Bauer a reptéren átmegy a kapun és a fémdetektor nem jelez, a biztonságiak adnak neki egy puskát.",
+	"Szun Cu (neves ókori kínai hadtörténész, katonai szakértő) azt írja: \"Ha az ellenséged gyenge, támadd meg. Ha erős, szövetkezz vele. Ha Jack Bauer az, neked véged.\"",
+	"Születésekor Jack Bauer nem világra jött. Hanem dühbe.",
+	"Ha a Google nem talál valamit, Jack Bauerhez fordul segítségért.",
+	"Ne is próbálj könyörögni Jacknek, hogy öljön meg. Akkor inkább megöli a feleségedet. Nehogy má' megmondják neki, mit csináljon!",
+	"Jack Baruer egy szemesztert tanított a Harvardon. A téma ez volt: \"Időbeosztás - amit egy nap besűríthetsz\".",
+	"Ha Jack Bauer egy szobába kerülne Hitlerrel, Sztálinnal és Nina Myersszel, és a pisztolyában csak két golyó lenne, lelőné Hitlert és Sztálint. Szemtanúként senkinek se mondhassák el utólag, hogyan bánt el Ninával.",
+	"Jézus meghalt és harmadnapra feltámadott. Ugyanez Jack Bauernek egy órába sem tellett. És ő kétszer megcsinálta.",
+	"A terroristák körében a három leggyakoribb halálozási ok mind Jack Bauer.",
+	"Ha Jack Bauer meglát egy terroristát, amint épp öngyilkos bombamerényletre készül, előbb kinyírja a bombát. Nehogy az végezzen meg a terroristával helyette.",
+	"'Jack Bauer' - a szó arab nyelven azt jelenti: 'nekem lőttek'.",
+	"Ha Batman bajba kerül, az égre vetíti egy reflektorral a Jack Bauer-jelet.",
+	"Jack a sivatagban sétált. Megharapta egy csörgőkígyó. A kígyó azonnal elpusztult.",
+	"Amikor a Time magazin meghirdeti \"az év emberét*\", a címlap alján ott a lábjegyzet: \"* természetesen Jack Bauer-t nem számítva\".",
+	"Jack Bauer eltávolította a számítógépe billentyűzetéről az 'Escape' gombot. Neki arra sosem lesz szüksége.",
+	"Egy robbanás nem ölheti meg Jack Bauert, viszont eltakarítja a limlomot az útjából.",
+	"Jack Bauer fél 11 után is kaphat reggelit a McDonald'sban.",
+	"Quentin Tarantinót kérték fel a Jack Bauer életrajzi film rendezésére. Nem vállalta. Túl erőszakos - mondta.",
+	"Ha a válasz Jack Bauer, mindegy, mi volt a kérdés.",
+	"Ha a mumus aludni megy, bezárja a szobaajtót, nehogy Jack Bauer bejöjjön.",
+	"A reklámszünetekben Jack Bauernek jut ideje, hogy megoldja a Helyszínelők eseteit is.",
+	"Jack Bauer, ha siet, a sípszó előtt hagy üzenetet",
+	"Jack Bauer már annyi bűnözőt iktatott ki, hogy a CIA \"Most Wanted\" listáján az ötödik egy 18 éves maláj srác volt, aki illegálisan letöltötte a Tüskevár című filmet."
+];
+
+var getAFact = function() {
+	// check if we're out of facts to avoid an indexOutOfBounds exception
+	if(jack_bauer_facts.length === 0) {
+		// all the facts have been seen, tell user that they're done.
+		$("#fact").text("Chuck Norris can create infinite geeky facts about himself, but we can't! Check out our GitHub page to help add more!");
+	} else {
+		// there are still facts in the array, so picks a random one
+		var index = Math.floor(Math.random() * jack_bauer_facts.length);
+		$("#fact").text(jack_bauer_facts[index]);
+
+		// remove this fact from the facts list to avoid repeating facts
+		jack_bauer_facts.splice(index, 1);
+	}
+}
+
+// set up handler for when random fact button clicked
+$("#random_fact").click(getAFact);
+
+// initialize text
+getAFact();
+
+// google analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-43436805-11', 'auto');
+ga('require', 'displayfeatures');
+ga('require', 'linkid', 'linkid.js');
+ga('send', 'pageview');
